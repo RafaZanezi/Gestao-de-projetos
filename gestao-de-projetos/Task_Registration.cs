990d1ccdf90de63832342efb6487032a1dd0c13d
Marcos Veiga <mfvmfvfilho@gmail.com>
2019-12-05 11:27:00 -0300

    Comitando a branch acumulada da semana

---
+++ gestao-de-projetos/Task_Registration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+
+namespace gestao_de_projetos
+{
+    public partial class Task_Registration : MaterialForm
+    {
+        public Task_Registration()
+        {
+            InitializeComponent();
+            design.configMaterialSkin(this);
+        }
+
+        private void Task_Registration_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
