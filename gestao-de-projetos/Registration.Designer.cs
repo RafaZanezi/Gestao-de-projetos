b4654f7f77f2f9845e7aea914e18e8f45cf8471c
MarcosVeigaFF <mfvmfvfilho@gmail.com>
2019-12-11 19:14:30 -0200

    Erro Registration reparado

--- gestao-de-projetos/Registration.Designer.cs
+++ gestao-de-projetos/Registration.Designer.cs
@@ -176,6 +176,7 @@
             this.Controls.Add(this.nome_usu);
             this.Name = "Registration";
             this.Text = "Registration";
+            this.Load += new System.EventHandler(this.Registration_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
